8843b400cb1ebdb763cb7cfb562c76f2133ad36f
aiodo-lab <108018053+aiodo-lab@users.noreply.github.com>
2023-02-07 09:18:52 +0000

    Create write-video-frames-to-file.py

---
+++ utils/write-video-frames-to-file.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import cv2
+import imutils
+from glob import glob
+
+def create_dir(path):
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    except OSError:
+        print(f"Error: creating directory with name {path}")
+
+def save_frame(video_path, save_dir):
+    name = video_path.split("/")[-1].split(".")[0]
+    save_path = os.path.join(save_dir, name)
+    create_dir(save_path)
+    cap = cv2.VideoCapture(video_path)
+    idx = 0
+    while True:
+        ret, frame = cap.read()
+        if ret == False:
+            cap.release()
+            break
+        mask = np.zeros(frame.shape[:2], dtype = "uint8")
+        cv2.rectangle(mask, (0, 200), (1920, 1080), 255, -1) 
+        newImage = cv2.bitwise_and(frame, frame, mask = mask)
+        #newImage = imutils.resize(newImage, width=640)
+        
+        cv2.imwrite(f"{save_path}/{idx}.png", newImage)
+        idx += 1
+
+if __name__ == "__main__":
+    video_paths = glob("videos/*")
+    save_dir = "frames_1"
+    for path in video_paths:
+        save_frame(path, save_dir)
