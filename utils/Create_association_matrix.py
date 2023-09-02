152050a71e42e7a4507601a08e48ec9fcc3a0eec
aiodo-lab <108018053+aiodo-lab@users.noreply.github.com>
2023-09-02 08:15:16 +0100

    Update Create_association_matrix.py

--- utils/Create_association_matrix.py
+++ utils/Create_association_matrix.py
@@ -30,16 +30,17 @@ for frame in range(frames_in_video):
     # get all the pig bboxes and ear-biting bboxes that happen in this frame
     # I assume pig_detections_this_frame and ear_biting_events_this_frame are numpy arrays
     # with [frame,track_number,x,y,w,h]
+    
     bite_bbox_locs = np.where(all_eb_event_detections[:,0] == frame)
     ear_biting_events_this_frame = all_eb_event_detections.take(bite_bbox_locs, axis=0)[0]
     ear_biting_events_this_frame = ear_biting_events_this_frame.tolist()
     num_events = len(ear_biting_events_this_frame)
-    #num_events = np.shape(ear_biting_events_this_frame)[1]
+
     pig_bbox_locs = np.where(all_pig_detections[:,0] == frame)
     pig_detections_this_frame = all_pig_detections.take(pig_bbox_locs, axis=0)[0]
     pig_detections_this_frame = pig_detections_this_frame.tolist()
     num_pigs = len(pig_detections_this_frame)
-    #num_pigs = np.shape(pig_detections_this_frame)[1]
+
     # a matrix to hold the IOU between pig bboxes and EB events	
     # this will be used to find the assignments between pigs and EB events 	
     # each entry holds and assignment cost between pigs and EB events 	
@@ -47,15 +48,15 @@ for frame in range(frames_in_video):
     # Note - very important to ensure the matrix dimensions are the correct way around 	
     # i.e. events, then pigs in order for the assignment to work. I've tried to keep this 	
     # consistent across the rest of the code
+    
     association_matrix = np.ones((num_events,num_pigs)) * MAX_ASSIGNMENT_COST
     pig_detections_this_frame_x = np.array(pig_detections_this_frame) 
     ear_biting_events_this_frame_x = np.array(ear_biting_events_this_frame)
     for i in range(num_events): 
-        event_bbox = ear_biting_events_this_frame_x[i,2:6] # format (x1 y1, x2, y2)
+        event_bbox = ear_biting_events_this_frame_x[i,2:6]
         # print(event_bbox)
         for j in range(num_pigs): 
-            pig_bbox = pig_detections_this_frame_x[j,2:10] # Poly format (x1 y1, x2, y2, x3, y3, x4, y4)
-            # print(pig_bbox)
+            pig_bbox = pig_detections_this_frame_x[j,2:10] # detection in poly format (x1 y1, x2, y2, x3, y3, x4, y4)
             # the assignment cost is (1 - IOU) between the pig and ear-biting event bbox
             # we may need to use an if-statement here to only allow cases there IOU > 0.5 
             # to be sure that the pig is associated with the event... We can experiment with this...
@@ -63,8 +64,8 @@ for frame in range(frames_in_video):
             # IOU may not work well since it assumes both bboxes are similar in size. But in fact 
             # the EB bbox is much smaller. We might need to use the intersection with the EB box instead. 
             # Again more experiments needed...
+            
             pig_bbox = bt.transforms.poly2hbb(pig_bbox)
-            # print(pig_bbox)
             association_matrix[i,j] = 1-iou(event_bbox, pig_bbox)
             
     # now we solve a linear-assignment problem to find the optimal assignment 
